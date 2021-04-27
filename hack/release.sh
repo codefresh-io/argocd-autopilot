4feaf100cedda40f79dcf501c31c0a97f120fc33
roi.kramer <roi.kramer@codefresh.io>
2021-04-27 16:06:45 +0300

    working on cd

--- hack/release.sh
+++ hack/release.sh
@@ -13,9 +13,33 @@ if [[ -z "$GIT_TOKEN" ]]; then
     exit 1
 fi
 
+if [[ -z "$PRERELEASE" ]]; then
+    PRERELEASE=false
+fi
+
 if [[ "$?" == "0" ]]; then
     echo "on release branch: $GIT_BRANCH"
-    gh release create --repo $GIT_REPO
+    echo "uploading files:"
+    ls -1a ./dist/*.gz ./dist/*.sha256
+    echo ""
+
+    FILE=""
+    if [ -f "./docs/releases/release-$GIT_BRANCH.md" ]; then
+        FILE="./docs/releases/release-$GIT_BRANCH.md"
+        echo "using release notes file: ./docs/releases/release-$GIT_BRANCH.md"
+        echo ""
+    else
+        FILE="./docs/releases/default.md"
+        echo "using default release notes file: ./docs/releases/default.md"
+        echo ""
+    fi
+
+    if [[ "$PRE_RELEASE" ]]; then
+        echo "using pre-release"
+    fi
+
+    echo "running: gh release create --repo $GIT_REPO -t $GIT_BRANCH -F $FILE --target $GIT_BRANCH --prerelease $PRERELEASE ./dist/*.gz ./dist/*.sha256"
+    gh release create --repo $GIT_REPO -t $GIT_BRANCH -F $FILE --target $GIT_BRANCH --prerelease $PRERELEASE ./dist/*.gz ./dist/*.sha256
 else 
     echo "not on release branch: $GIT_BRANCH"
     exit 1
